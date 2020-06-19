2cf531c802fd94cbff45bd2858e7212122765bca
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-06-19 00:58:51 -0500

    refactor json responses

--- app/Http/Controllers/Api/CashRegisterController.php
+++ app/Http/Controllers/Api/CashRegisterController.php
@@ -10,32 +10,37 @@ use App\Http\Requests\StoreOpenDayRequest;
 
 class CashRegisterController extends Controller
 {
-    public function generateOpenResponse($resource = null, $status = 200, $message = 'Success') {
-        if($resource == null || $resource->closing_date != null) {
-            return response()->json([
-                'message' => $message,
-                'data' => [
-                    'type' => null,
-                    'opening_date'=> null,
-                    'opening_hour' => null,
-                    'previous_closing_value' => null,
-                    'opening_value' => null,
-                    'description'=> null,
-                ]
-            ], $status);
-        }
-
+    protected function generateCustomResponse($message = 'success', $data = [], $status = 200)
+    {
         return response()->json([
             'message' => $message,
-            'data' => [
+            'data' => $data
+        ]);
+    }
+
+    protected function generateOpenResponse($resource = null, $status = 200, $message = 'Success') {
+        $data =  [
+            'type' => 'cash_register',
+            'opening_date'=> null,
+            'opening_hour' => null,
+            'previous_closing_value' => null,
+            'opening_value' => null,
+            'description'=> null,
+        ];
+
+        if($resource != null && $resource->closing_date == null) {
+            $data = [
                 'type' => 'cash_register',
                 'opening_date'=> $resource->opening_date,
                 'opening_hour' => $resource->opening_hour,
                 'previous_closing_value' => $resource->previous_closing_value,
                 'opening_value' => $resource->opening_value,
                 'description'=> $resource->description,
-            ]
-        ], $status);
+            ];
+        }
+
+        return $this->generateCustomResponse($message, $data, $status);
+
     }
 
     public function getOpenCashRegister()
@@ -44,12 +49,11 @@ class CashRegisterController extends Controller
         return $this->generateOpenResponse($lastRegister);
     }
 
-
     public function storeOpenCashRegister(StoreOpenDayRequest $request)
     {
         $openCash = CashRegister::create($request->all());
         $openCash->save();
-         return $this->generateOpenResponse($openCash, 201, 'CashRegister was open with success');
+        return $this->generateOpenResponse($openCash, 201, 'Cash register was open with success');
     }
 
     public function storeCloseCashRegister(StoreCloseDayRequest $request)
@@ -58,23 +62,20 @@ class CashRegisterController extends Controller
         $lastRegister->update($request->all());
         $lastRegister->save();
 
-        return response()->json([
-            'message' => 'Cash register was closed with success',
-            'data' => []
-        ]);
+        return $this->generateCustomResponse('Cash register was closed with success');
     }
 
     public function getCloseCashRegister()
     {
         $lastRegister = CashRegister::latest()->first();
-        return response()->json([
-            'message' => 'Success!',
-            'data' => [
-                'type' => 'cash_register',
-                'closing_value'=> $lastRegister->closing_value,
-                'card_value' => $lastRegister->card_value,
-                'value' => $lastRegister->closing_value - $lastRegister->card_value
-            ]
+        if($lastRegister == null || $lastRegister->closing_date == null) {
+           return $this->generateCustomResponse('No se puede mostrar esta información');
+        }
+        return $this->generateCustomResponse('Success', [
+            'type' => 'cash_register',
+            'closing_value'=> $lastRegister->closing_value,
+            'card_value' => $lastRegister->card_value,
+            'value' => $lastRegister->closing_value - $lastRegister->card_value
         ]);
     }
 
