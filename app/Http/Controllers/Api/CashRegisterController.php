0af211e0a2696023b05fdc6d3655ef1f93a3839f
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-06-18 21:15:15 -0500

    add store open day method

--- app/Http/Controllers/Api/CashRegisterController.php
+++ app/Http/Controllers/Api/CashRegisterController.php
@@ -4,40 +4,53 @@ namespace App\Http\Controllers\Api;
 
 use App\CashRegister;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreOpenDayRequest;
+use Dotenv\Validator;
+use Illuminate\Http\Request;
 
 
 class CashRegisterController extends Controller
 {
-    public function getOpenDay()
-    {
-        $lastRegister = CashRegister::latest()->first();
-
-        return $this->generateOpenResponse($lastRegister);
-
-    }
-
-    public function generateOpenResponse($resource) {
+    public function generateOpenResponse($resource = null, $status = 200, $message = 'Success') {
         if($resource == null) {
             return response()->json([
+                'message' => $message,
                 'data' => [
+                    'type' => null,
                     'opening_date'=> null,
                     'opening_hour' => null,
                     'previous_closing_value' => null,
                     'opening_value' => null,
                     'description'=> null,
                 ]
-            ]);
+            ], $status);
         }
 
         return response()->json([
+            'message' => $message,
             'data' => [
+                'type' => 'cash_register',
                 'opening_date'=> $resource->opening_date,
                 'opening_hour' => $resource->opening_hour,
                 'previous_closing_value' => $resource->previous_closing_value,
                 'opening_value' => $resource->opening_value,
                 'description'=> $resource->description,
             ]
-        ]);
+        ], $status);
+    }
+
+    public function getOpenDay()
+    {
+        $lastRegister = CashRegister::latest()->first();
+        return $this->generateOpenResponse($lastRegister);
+    }
+
+
+    public function storeOpenDay(StoreOpenDayRequest $request)
+    {
+        $openCash = CashRegister::create($request->all());
+        $openCash->save();
+         return $this->generateOpenResponse($openCash, 201, 'CashRegister was open with success');
     }
 
 }
---
+++ app/Http/Requests/StoreOpenDayRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreOpenDayRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'opening_date' => 'required|date',
+            'opening_hour' => 'required|date_format:H:i',
+            'previous_closing_value' => 'required|integer',
+            'opening_value' => 'required|integer',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Opss! something is wrong with request body.',
+            'errors' => $validator->errors()
+        ], 403));
+    }
+
+
+}
